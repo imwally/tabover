0ff6f5a49e5e0266e8777c9e654411fcf2b71d70
Wally <wally@imwally.net>
2015-10-18 23:50:00 -0400

    go back to non-struct version

--- main.c
+++ main.c
@@ -8,19 +8,20 @@
 #define NEXT 1
 #define PREV -1
 
-
 static xcb_connection_t *conn; // connection to X
 static xcb_screen_t *scrn;     // main screen
+static xcb_window_t *ws;       // window ids
+static int wsel = 0;           // current selected window
+static struct termios torig;   // original terminal settings
 
-static const uint32_t mask = (XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT |
-			      XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY);
+const uint32_t mask = (XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT |
+		       XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY);
 
-struct termios torig;   // original terminal settings
-
+// maybe hold all of the window information in an array of window
+// structs?
 struct window {
     xcb_window_t id;
     char *name;
-    char *instance;
     int desktop;
     int selected;
 };
@@ -140,6 +141,27 @@ desktop_of_window(xcb_window_t w)
     return desktop;
 }
 
+int
+client_list(xcb_window_t w, xcb_window_t **windows)
+{
+    xcb_get_property_cookie_t c;
+    xcb_get_property_reply_t *r;
+    int wn = 0;
+
+    xcb_atom_t atom = get_atom("_NET_CLIENT_LIST");
+    
+    c = xcb_get_property(conn, 0, w, atom, XCB_ATOM_WINDOW, 0L, 32L);
+    r = xcb_get_property_reply(conn, c, NULL);
+
+    if (r) {
+	*windows = malloc(sizeof(xcb_window_t) * r->length);
+	memcpy(*windows, xcb_get_property_value(r), sizeof(xcb_window_t) * r->length);
+	wn = r->length;
+    }
+    
+    free(r);
+    return wn;
+}
 
 void
 send_client_message(xcb_window_t destination, xcb_window_t window,
@@ -192,41 +214,46 @@ select_window(xcb_window_t window)
     switch_to_desktop(desktop);
 }
 
-int
-open_windows(xcb_window_t screen, struct window **windows)
+void
+print_selection(int wn, xcb_window_t *windows, int wsel)
 {
-    xcb_get_property_cookie_t c;
-    xcb_get_property_reply_t *r;
-    xcb_window_t *client_list;
-    int wn = 0;
-      
-    xcb_atom_t atom = get_atom("_NET_CLIENT_LIST");
+    char *wname, *wclass, *winstance = 0;
+    int wdesktop, i = 0;
     
-    c = xcb_get_property(conn, 0, screen, atom, XCB_ATOM_WINDOW, 0L, 32L);
-    r = xcb_get_property_reply(conn, c, NULL);
-
-    if (r) {
-	wn = r->length;
-	client_list = xcb_get_property_value(r);
-	*windows = malloc(sizeof(struct window) * wn);
-
-	for (int i = 0; i < wn; ++i) {
-	    xcb_window_t id = client_list[i];
-	    char *wclass = get_prop_string(XCB_ATOM_WM_CLASS, id);
-	    char *name = get_prop_string(XCB_ATOM_WM_NAME, id);
-	    int desktop = desktop_of_window(id)+1;
-
-	    windows[i] = malloc(sizeof(struct window));
-	    windows[i]->id = id;
-	    windows[i]->name = name;
-	    windows[i]->desktop = desktop;
-	    windows[i]->instance = &wclass[strlen(wclass)+1];
-	    windows[i]->selected = 0;
+    for (i = 0; i < wn; i++) {
+	if (wsel == i) {
+	    printf(">");
 	}
+	
+	wname     = get_prop_string(XCB_ATOM_WM_NAME, windows[i]);
+	wclass    = get_prop_string(XCB_ATOM_WM_CLASS, windows[i]);
+	winstance = &wclass[strlen(wclass)+1];
+	wdesktop  = desktop_of_window(windows[i])+1;
+
+	printf(" %d: [%s] %s\n", wdesktop, winstance, wname);
+    }
+}
+  
+void
+cycle_selection(int direction, int wn, xcb_window_t *windows, int select)
+{
+    system("clear");
+
+    wsel += direction;
+
+    if (select) {
+	select_window(windows[wsel]);
+    }
+
+    if (wsel >= wn) {
+	wsel = 0;
     }
     
-    free(r);
-    return wn;
+    if (wsel < 0) {
+	wsel = wn-1;
+    }
+
+    print_selection(wn, windows, wsel);
 }
 
 void
@@ -237,24 +264,23 @@ cleanup()
 	perror("buffer input");
     }
     
+    free(ws);
     xcb_disconnect(conn);
 }
 
 int
 main(int argc, char **argv)
 {
-    int wn = 0;  // number of windows open
     char ch = 0; // character pressed
-
+    int wn = 0;  // number of windows
+    
     // Setup connection to X and grab screen
     init_xcb(&conn);
     get_screen(conn, &scrn);
 
-    // Get a list of open windows via _NET_CLIENT_LIST of the root
-    // screen
-    struct window *windows;
-    wn = open_windows(scrn->root, &windows);
-    
+    // Get a list of windows via _NET_CLIENT_LIST of the root screen
+    wn = client_list(scrn->root, &ws);
+
     // Save original stdin settings into torig
     if (-1 == tcgetattr(0, &torig)) {
 	perror("tcgetattr");
@@ -267,5 +293,33 @@ main(int argc, char **argv)
 	return -1;
     }
 
-
+    // Invocation: start window selection at zero, or the first window
+    cycle_selection(0, wn, ws, 0);
+    
+    // Cycle window selection forward when TAB or j is press and
+    // backwards when ` or k is pressed. Return activates window.
+    while (1) {
+	ch = fgetc(stdin);
+	switch (ch) {
+	case '\t':
+	    cycle_selection(NEXT, wn, ws, 0);
+	    break;
+	case 'j':
+	    cycle_selection(NEXT, wn, ws, 0);
+	    break;
+	case '`':
+	    cycle_selection(PREV, wn, ws, 0);
+	    break;
+	case 'k':
+	    cycle_selection(PREV, wn, ws, 0);
+	    break;
+	case 'q':
+	    cleanup();
+	    return 0;
+	case '\r':
+	    cycle_selection(0, wn, ws, 1);
+	    cleanup();
+	    return 0;
+	}
+    }
 }
